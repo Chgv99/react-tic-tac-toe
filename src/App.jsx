3b642feb069c665710152d51eb90b111274c1aa9
Christian G <christiangv99@gmail.com>
2025-10-29 20:43:34 +0000

    Add winner check and status

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,38 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [nextSymbol, setNextSymbol] = useState("X");
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = nextSymbol + "'s turn.";
+  }
 
   function checkSymbol() {
     switch (nextSymbol) {
@@ -24,7 +53,9 @@ export default function Board() {
   }
 
   function handleClick(index) {
-    if (squares[index]) return;
+    if (squares[index] || winner) {
+      return;
+    }
     const nextSquares = squares.slice();
     nextSquares[index] = nextSymbol;
     setSquares(nextSquares);
@@ -33,6 +64,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
